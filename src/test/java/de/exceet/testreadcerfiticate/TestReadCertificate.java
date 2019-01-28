76c25dbf31dd7759ea630ed2d705b34d85be1080
van Spran Jakob <jvansprang@exceet.de>
2019-01-28 11:23:01 +0100

    Added a test for the write() method to the Test and added a new certificate for that test to the test resources.

--- src/test/java/de/exceet/testreadcerfiticate/TestReadCertificate.java
+++ src/test/java/de/exceet/testreadcerfiticate/TestReadCertificate.java
@@ -7,17 +7,22 @@ import org.junit.Test;
 import javax.security.cert.CertificateException;
 import java.io.File;
 import java.io.IOException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
 public class TestReadCertificate{
     @Test
     public void test(){
-        List<String> test = new ArrayList<String>();
+        //---- Test read() ----//
+        List<String> testRead = new ArrayList<String>();
         try {
-            ReadCertificate tc = new ReadCertificate();
-            test = tc.read(new File("src/test/resources/testcertificate.crt"));
+            ReadCertificate rc = new ReadCertificate();
+            testRead = rc.read(new File("src/test/resources/testCertificate.crt"));
         }catch (IOException ioe){
             System.out.println(ioe);
             System.out.println("IOException");
@@ -25,18 +30,56 @@ public class TestReadCertificate{
             System.out.println(ce);
             System.out.println("Missing the certificate test file or wrong path file");
         }
-        System.out.println("test fist certificate");
-        assert test.get(1).equals("Version: 1");
-        assert test.get(2).equals("Serial Number: 1234567890");
-        assert test.get(3).equals("Issuer: CN=ca_name");
-        assert test.get(4).equals("Subject: CN=owner_name");
-        if(Integer.valueOf(test.get(6)) == 0){
-            assert test.get(5).equals("Validity: Mon Jan 21 00:00:00 CET 2019 - Tue Jan 28 10:05:06 CET 2020 - The certificate is valid.");
+        //----+
+        System.out.println("Test read() function");
+        assert testRead.get(1).equals("Version: 1");
+        assert testRead.get(2).equals("Serial Number: 1234567890");
+        assert testRead.get(3).equals("Issuer: CN=ca_name");
+        assert testRead.get(4).equals("Subject: CN=owner_name");
+        if(Integer.valueOf(testRead.get(6)) == 0){
+            assert testRead.get(5).equals("Validity: Mon Jan 21 00:00:00 CET 2019 - Tue Jan 28 10:05:06 CET 2020 - The certificate is valid.");
         }else{
-            assert test.get(5).equals("Validity: Mon Jan 21 00:00:00 CET 2019 - Tue Jan 28 10:05:06 CET 2020 - The certificate is not valid.");
+            assert testRead.get(5).equals("Validity: Mon Jan 21 00:00:00 CET 2019 - Tue Jan 28 10:05:06 CET 2020 - The certificate is not valid.");
         }
-        assert test.get(8).equals("Hash Code: -40609");
-        assert test.get(9).equals("Signature algorithm: SHA256withRSA. The algorithm type is RSA.");
-        System.out.println("first certificate tested");
+        assert testRead.get(8).equals("Hash Code: -40609");
+        assert testRead.get(9).equals("Signature algorithm: SHA256withRSA. The algorithm type is RSA.");
+        System.out.println("Completed testing read() function");
+        //---- +----------+ ----//
+
+        //---- Test write() ----//
+        List<String> testWrite = new ArrayList<String>();
+        String iName = "CN=ca" + (int)(Math.random()*100);
+        String sName = "CN=owner" + (int)(Math.random()*100);
+        KeyPairGenerator keyGen;
+        KeyPair keyPair;
+        Date now = new Date();
+        Date eDate = now;
+        eDate.setTime(now.getTime() + 1000000L);
+        long serNumber = now.getTime();
+        try {
+            keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(512);
+            keyPair = keyGen.generateKeyPair();
+            try {
+                ReadCertificate wc = new ReadCertificate();
+                wc.write(new File("src/test/resources/testGeneratedCertificate.crt"), iName, sName, keyPair, serNumber, now, eDate, "SHA256withRSA");
+                testWrite = wc.read(new File("src/test/resources/testGeneratedCertificate.crt"));
+            } catch (IOException ioe) {
+                System.out.println(ioe);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }catch(NoSuchAlgorithmException nSAE) {
+            System.out.println(nSAE);
+        }
+        //----+
+        System.out.println("Test write() function");
+        assert testWrite.get(1).equals("Version: 1");
+        assert testWrite.get(2).equals("Serial Number: "+ serNumber);
+        assert testWrite.get(3).equals("Issuer: " + iName);
+        assert testWrite.get(4).equals("Subject: " + sName);
+        assert testWrite.get(9).equals("Signature algorithm: SHA256withRSA. The algorithm type is RSA.");
+        System.out.println("Completed testing write() function");
+        //---- +----------+ ----//
     }
 }
